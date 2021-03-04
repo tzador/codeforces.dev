0eed36e38ffc99c003c7240634ab79b02b4ef20a
Tim Zadorozhny <tzador@gmail.com>
2021-03-04 11:09:11 +0200

    Added all the CodeMirror themes

--- src/Edit.svelte
+++ src/Edit.svelte
@@ -5,29 +5,38 @@
 
   import { Link } from "svelte-routing";
   import { onMount } from "svelte";
+  import { themes } from "./themes.js";
 
   let textarea;
+  let editor;
+  let theme;
 
   onMount(() => {
-    const editor = CodeMirror.fromTextArea(textarea, {
+    editor = CodeMirror.fromTextArea(textarea, {
       lineNumbers: true,
       styleActiveLine: true,
       matchBrackets: true,
     });
     editor.setOption("theme", "pastel-on-dark");
+    theme = localStorage.getItem("theme") || "pastel-on-dark";
+    changeTheme();
   });
+
+  function changeTheme() {
+    localStorage.setItem("theme", theme);
+    editor.setOption("theme", theme);
+  }
 </script>
 
 <div class="header row center">
   <div class="gap" />
   <Link to="/">codeforces.dev</Link>
   <div class="grow" />
-  <select>
-    <option value="pastel-on-dark">pastel-on-dark</option>
-    <option value="pastel-on-dark">pastel-on-dark</option>
-    <option value="pastel-on-dark">pastel-on-dark</option>
-    <option value="pastel-on-dark">pastel-on-dark</option>
-    <option value="pastel-on-dark">pastel-on-dark</option>
+  <!-- svelte-ignore a11y-no-onchange -->
+  <select bind:value={theme} on:change={changeTheme}>
+    {#each themes as theme (theme)}
+      <option value={theme}>{theme}</option>
+    {/each}
   </select>
   <div class="gap" />
   <select>
@@ -35,6 +44,8 @@
     <option value="java">java</option>
     <option value="python">python</option>
   </select>
+  <div class="gap" />
+  <button>Run All</button>
   <div class="gap" />
 </div>
 <div class="left" />
@@ -51,7 +62,8 @@
     right: 0;
     height: 3em;
     line-height: 3em;
-    box-shadow: 0 0 1em rgba(0, 0, 0, 0.5);
+    box-shadow: 0 0 0.5em rgba(0, 0, 0, 0.125);
+    z-index: 11;
   }
   .left {
     position: fixed;
